32be24681f51434f4d925b263cbae41aea156118
Felipe Goncalves <felipefgoncalves@gmail.com>
2026-02-04 16:34:51 +0000

    Initial Commit

---
+++ iOS SGTM/iOS_SGTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_SGTMApp.swift
+//  iOS SGTM
+//
+//  Created by Felipe on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct iOS_SGTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
